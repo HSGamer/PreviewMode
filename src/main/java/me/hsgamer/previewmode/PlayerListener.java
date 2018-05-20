acff0e3d0ecb54e4b628d3c6e7d8499372a7cdb7
huynhqtienvtag <huynhqtienvtag@gmail.com>
2018-05-20 14:00:42 +0700

    Some spaces Added IsEnabled config

--- src/main/java/me/hsgamer/previewmode/PlayerListener.java
+++ src/main/java/me/hsgamer/previewmode/PlayerListener.java
@@ -7,14 +7,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class PlayerListener implements Listener {
+
+    private PreviewMode previewMode = new PreviewMode();
+
     @EventHandler
     public void onRegistered(LoginEvent e) {
-        if (!(e.getPlayer().hasPermission("previewmode.bypass") || e.getPlayer().isOp())) {
-            e.getPlayer().setGameMode(GameMode.SPECTATOR);
-            e.getPlayer().sendMessage(ChatColor.RED + "You are in Preview Mode");
-        }
-        else {
-            e.getPlayer().sendMessage(ChatColor.GREEN + "You are bypassed PreviewMode. Enjoy your game");
+        if (previewMode.IsEnabled) {
+            if (!(e.getPlayer().hasPermission("previewmode.bypass") || e.getPlayer().isOp())) {
+                e.getPlayer().setGameMode(GameMode.SPECTATOR);
+                e.getPlayer().sendMessage(ChatColor.RED + "You are in Preview Mode");
+            } else {
+                e.getPlayer().sendMessage(ChatColor.GREEN + "You are bypassed PreviewMode. Enjoy your game");
+            }
         }
     }
 }
--- src/main/java/me/hsgamer/previewmode/PreviewMode.java
+++ src/main/java/me/hsgamer/previewmode/PreviewMode.java
@@ -6,15 +6,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PreviewMode extends JavaPlugin implements Listener {
 
+    public boolean IsEnabled;
+
     @Override
     public void onEnable() {
+
         this.saveDefaultConfig();
+        IsEnabled = this.getConfig().getBoolean("enabled");
+
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Preview Mode has beed Enabled");
+
     }
 
     @Override
     public void onDisable() {
+
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "Preview Mode has been Disabled");
+        this.saveConfig();
+
     }
+
 }
